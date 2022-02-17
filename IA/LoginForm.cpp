1e608c61f94806bdd14bf7d3576c057b9e61b83d
victormayzus <victor.mayzus@gmail.com>
2022-02-17 23:30:36 +0100

    LoginForm.cpp added code Changed Properties to work on Windows

--- IA/LoginForm.cpp
+++ IA/LoginForm.cpp
@@ -1,2 +1,11 @@
 #include "LoginForm.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThreadAttribute]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	IA::LoginForm form;
+	Application::Run(% form);
+}
